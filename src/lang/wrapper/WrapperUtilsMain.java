18d1ec9b085d6a26463bf2d47aaf0156fea5b552
hyejin <phj981231@naver.com>
2024-07-10 21:40:27 +0900

    [20240710] 자바 중급 #5 래퍼 클래스의 주요 메서드와 기능

---
+++ src/lang/wrapper/WrapperUtilsMain.java
@@ -0,0 +1,22 @@
+package lang.wrapper;
+
+/**
+ * 래퍼 클래스 - 주요 메서드와 기능
+ */
+public class WrapperUtilsMain {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf(10); // 숫자, 래퍼 객체 변환
+        Integer i2 = Integer.valueOf("10"); // 문자열, 래퍼 객체 변환
+
+        int i3 = Integer.parseInt("10"); // 문자열 전용, 기본형 반환
+
+        // 비교
+        int compareResult = i1.compareTo(20);
+        System.out.println("compareResult = " + compareResult);
+
+        // 산술
+        System.out.println("sum : " + Integer.sum(10, 20));
+        System.out.println("min : " + Integer.min(10, 20));
+        System.out.println("max : " + Integer.max(10, 20));
+    }
+}
---
+++ src/lang/wrapper/WrapperVsPrimitive.java
@@ -0,0 +1,36 @@
+package lang.wrapper;
+
+/**
+ * 래퍼 클래스 - 주요 메서드와 기능
+ */
+public class WrapperVsPrimitive {
+    public static void main(String[] args) {
+        int iteration = 1_000_000_000; // 반복 횟수 설정, 10억
+        long startTime, endTime;
+
+        // 기본형 long 사용
+        long sumPrimitive = 0;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iteration; i++) {
+            sumPrimitive += i;
+        }
+
+        endTime = System.currentTimeMillis();
+
+        System.out.println("sumPrimitive = " + sumPrimitive);
+        System.out.println("기본 자료형 long 실행 시간 : " + (endTime- startTime) + "ms");
+
+
+        // 래퍼 클래스 Long 사용
+        Long sumWrapper = 0L;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iteration; i++) {
+            sumWrapper += i; // 오토 박싱 발생
+        }
+
+        endTime = System.currentTimeMillis();
+
+        System.out.println("sumPrimitive = " + sumWrapper);
+        System.out.println("기본 자료형 long 실행 시간 : " + (endTime- startTime) + "ms");
+    }
+}
