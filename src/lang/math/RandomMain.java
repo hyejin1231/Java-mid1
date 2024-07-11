02349885faafb5273477669ba094b30b2849fa49
hyejin1231 <phj981231@naver.com>
2024-07-11 12:42:38 +0900

    [20240711] 자바 중급 #5. Math, Random 클래스

---
+++ src/lang/math/RandomMain.java
@@ -0,0 +1,32 @@
+package lang.math;
+
+import java.util.Random;
+
+/**
+ * Random 클래스
+ */
+public class RandomMain
+{
+	public static void main(String[] args)
+	{
+		Random random = new Random();
+//		Random random = new Random(1); // seed 가 같으면 Random 의 결과가 같다.
+		
+		int randomInt = random.nextInt();
+		System.out.println("randomInt = " + randomInt);
+		
+		double randomDouble = random.nextDouble();// 0.0d ~ 1.0d
+		System.out.println("randomDouble = " + randomDouble);
+		
+		boolean randomBoolean = random.nextBoolean();
+		System.out.println("randomBoolean = " + randomBoolean);
+		
+		// 범위 조회
+		int randomRange1 = random.nextInt(10);// 0 ~ 9 출력
+		System.out.println("0 ~ 9 =" + randomRange1);
+		
+		int randomRange2 = random.nextInt(10) + 1; // 1 ~ 10 출력
+		System.out.println("1 ~ 10 = " + randomRange2);
+		
+	}
+}
