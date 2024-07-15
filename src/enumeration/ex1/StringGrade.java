789edf5be096c3db70b76d0e37cd6782f8c51b6b
hyejin <phj981231@naver.com>
2024-07-15 22:14:13 +0900

    [20240715] 자바 중급 색션5. 문자열과 타입 안정성2

---
+++ src/enumeration/ex1/StringGrade.java
@@ -0,0 +1,12 @@
+package enumeration.ex1;
+
+/**
+ * 열거형 Enum
+ * - 문쟈열과 타입 안정성 2
+ */
+public class StringGrade {
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+
+}
---
+++ src/enumeration/ex1/StringGradeEx1_1.java
@@ -0,0 +1,21 @@
+package enumeration.ex1;
+
+/**
+ * 열거형 Enum
+ * - 문쟈열과 타입 안정성 2
+ */
+public class StringGradeEx1_1 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(StringGrade.BASIC, price);
+        int gold = discountService.discount(StringGrade.GOLD, price);
+        int diamond = discountService.discount(StringGrade.DIAMOND, price);
+
+        System.out.println("BASIC 등급의 할인 금액 : " + basic);
+        System.out.println("GOLD 등급의 할인 금액 : " + gold);
+        System.out.println("DIAMOND 등급의 할인 금액 : " + diamond);
+
+    }
+}
