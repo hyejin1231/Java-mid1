39abe79d65e29437000feaa4e199074441a5a31e
hyejin1231 <phj981231@naver.com>
2024-07-29 12:25:58 +0900

    [20240729] 자바 중급 #8. - 지역 클래스 : 지역 변수 캡쳐 2

--- src/nested/local/LocalOuterV3.java
+++ src/nested/local/LocalOuterV3.java
@@ -1,5 +1,7 @@
 package nested.local;
 
+import java.lang.reflect.Field;
+
 /**
  * 지역 클래스
  */
@@ -31,5 +33,13 @@ public class LocalOuterV3 {
         LocalOuterV3 localOuterV1 = new LocalOuterV3();
         Printer printer = localOuterV1.process(2);
         printer.print();
+
+        // 추가
+        System.out.println("필드 확인");
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields)
+        {
+            System.out.println("field = " + field); // 캡쳐한 값들이 필드로 들어있는걸 확인할 수 있다.
+        }
     }
 }
