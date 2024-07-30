6325c45a6e74b151a3ee8442888ec2609ee331b0
hyejin1231 <phj981231@naver.com>
2024-07-30 12:21:49 +0900

    [20240730] 자바 중급 #8. - 문제와 풀이 1

---
+++ src/nested/test/OuterClass.java
@@ -0,0 +1,11 @@
+package nested.test;
+
+public class OuterClass
+{
+	static class NestedClass {
+		void hello()
+		{
+			System.out.println("NestedClass.hello");
+		}
+	}
+}
---
+++ src/nested/test/OuterClassMain.java
@@ -0,0 +1,10 @@
+package nested.test;
+
+public class OuterClassMain
+{
+	public static void main(String[] args)
+	{
+		OuterClass.NestedClass nestedClass = new OuterClass.NestedClass();
+		nestedClass.hello();
+	}
+}
