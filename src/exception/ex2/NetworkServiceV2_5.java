47762a12bdceeeeba88da1d3125ab9fe3df4a67b
hyejin <phj981231@naver.com>
2024-08-02 22:58:51 +0900

    [20240802] 자바 중급 색션10. - 예외 처리 도입 5 - finally

---
+++ src/exception/ex2/NetworkServiceV2_5.java
@@ -0,0 +1,27 @@
+package exception.ex2;
+
+/**
+ * 예외 처리 도입 5 - finally
+ */
+public class NetworkServiceV2_5
+{
+    public void sendMessage(String data) throws NetworkClientExceptionV2
+    {
+        String address = "http://example.com";
+        NetworkClientV2 client = new NetworkClientV2(address);
+
+        client.initError(data); // 추가
+
+        try {
+            // 정상 흐름
+            client.connect();
+            client.send(data);
+        } catch (NetworkClientExceptionV2 exceptionV2) {
+            // 예외 흐름 -> 근데 이건 다른 예외가 발생하면 잡을 수 없다,,
+            System.out.println("[오류] 코드 : " + exceptionV2.getErrorCode() + ", 메시지 : " + exceptionV2.getMessage());
+        }finally {
+            client.disconnect();
+        }
+
+    }
+}
